357f0252ef789a4e94c56b11398e5e997a03d5a7
Arman <armangolkar75@gmail.com>
2024-01-23 13:36:37 -0700

    crypto Pricelist added

--- index.js
+++ index.js
@@ -9,3 +9,37 @@ const scroll=document.getElementById("scroll")
 scroll.addEventListener('click',() =>{
     document.querySelector(".get-started").scrollIntoView({behavior: 'smooth'})
 })
+
+
+fetch('https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2Ctether%2Cethereum%2Clitecoin%2Ccardano%2Cdogecoin&vs_currencies=usd&include_24hr_change=true')
+    .then(res => res.json())
+    .then(json => {
+        const container = document.querySelector('.Price-container');
+        const coins = Object.getOwnPropertyNames(json);
+        var containerHtml= ``
+
+        for (let coin of coins) {
+            const coinInfo = json[`${coin}`];
+            console.log(coinInfo)
+            const price = coinInfo.usd;
+            const change = coinInfo.usd_24h_change.toFixed(5);
+            
+
+            containerHtml+= `
+                <div class="coin ${change < 0 ? 'falling' : 'rising'}">
+                    <div class="coin-logo">
+                        <img src="images/${coin}.png">
+                    </div>
+                    <div class="coin-name">
+                        <h3>${coin}</h3>
+                        <span>/USD</span>
+                    </div>
+                    <div class="coin-price">
+                        <span class="price">$${price}</span>
+                        <span class="change">${change}</span>
+                    </div>
+                </div>
+        `;
+        }
+        container.innerHTML=containerHtml
+    });
